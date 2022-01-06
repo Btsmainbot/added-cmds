ecba8b60bc88723a483c2f6f4698a2c41dbda324
NAB <87281576+Nabworks@users.noreply.github.com>
2022-01-06 22:51:49 +0200

    Create happymod.ts

---
+++ src/lib/happymod.ts
@@ -0,0 +1,21 @@
+/**
+
+ * Get modded APK from happymod.
+
+ * @param {string} query 
+
+ * @returns {Promise<object>} 
+
+ */
+
+const happymod = (query) => new Promise((resolve, reject) => {
+
+    console.log(`Searching for ${query}...`)
+
+    fetchJson(`https://tobz-api.herokuapp.com/api/happymod?q=${query}&apikey=${config.tobz}`)
+
+        .then((result) => resolve(result))
+
+        .catch((err) => reject(err))
+
+})
