8a1eec1e87d4f5723bceff74ec63cf2b82451dfc
Btsmainbot <101016939+Btsmainbot@users.noreply.github.com>
2022-03-11 18:25:13 +0200

    Update command.d.ts

--- src/typings/command.d.ts
+++ src/typings/command.d.ts
@@ -34,3 +34,72 @@ export type TCategory =
     | 'category'
     | 'weeb'
     | 'nsfw'
+import MessageHandler from '../Handlers/MessageHandler'
+
+import { WAClient } from '../lib/WAClient'
+
+import { IParsedArgs, ISimplifiedMessage } from './'
+
+export interface ICommand {
+
+    client?: WAClient
+
+    handler?: MessageHandler
+
+    run(M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void | never> | void | never
+
+    config: {
+
+        adminOnly?: boolean
+
+        aliases?: string[]
+
+        description?: string
+
+        command: string
+
+        id?: string
+
+        category?: TCategory
+
+        usage?: string
+
+        dm?: boolean
+
+        baseXp?: number
+
+        gold?: number
+
+        modsOnly?: boolean
+
+    }
+
+}
+
+export type TCategory =
+
+   
+
+    | 'bots'
+
+    | 'config'
+
+    | 'dev'
+
+    | 'fun'
+
+    | 'games'
+
+    | 'educative'
+
+    | 'general'
+
+    | 'media'
+
+    | 'moderation'
+
+    | 'category'
+
+    | 'weeb'
+
+    | 'nsfw'
